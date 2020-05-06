21112acbf12a0d8f60308f5154524db3ad038123
AT <bigfinn82@gmail.com>
2020-05-06 18:38:39 +0400

    Move autodiscover to adin view.

--- auth_demo/admin.py
+++ auth_demo/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from auth_demo.models.user import User
 
+admin.autodiscover()
+
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
--- auth_demo/api/views.py
+++ auth_demo/api/views.py
@@ -1,7 +1,6 @@
 
 from django.contrib.auth.models import Group
 from django.contrib import admin
-admin.autodiscover()
 
 from rest_framework import generics, permissions, serializers
 from oauth2_provider.contrib.rest_framework import TokenHasReadWriteScope, TokenHasScope
